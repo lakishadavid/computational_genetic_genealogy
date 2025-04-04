91fda51fae79125aa7f9a22b74ead477a891c4c1
LaKisha David <lakishatdavid@gmail.com>
2025-04-04 05:12:26 -0500

    Update class data files for JupyterLite application

--- build_jupyter.sh
+++ build_jupyter.sh
@@ -34,10 +34,6 @@ echo "Copying Lab notebooks ($LAB_SRC_DIR/Lab*.ipynb) to $STAGING_DIR..."
 # If you have other 'Lab*' files/dirs you want, change back to Lab*
 cp "$LAB_SRC_DIR"/Lab*.ipynb "$STAGING_DIR/"
 
-echo "Copying class data ($DATA_SRC_DIR) to $STAGING_DIR/class_data..."
-# Use trailing slash on source to copy contents
-cp -r "$DATA_SRC_DIR"/. "$STAGING_DIR/class_data/"
-
 # 4. Run the JupyterLite build command directly into DEPLOY_DIR
 echo "Running JupyterLite build directly into $DEPLOY_DIR using $ROOT_CONFIG_FILE..."
 # NOTE: Output dir is now DEPLOY_DIR
@@ -46,9 +42,41 @@ poetry run jupyter lite build \
     --contents "$STAGING_DIR" \
     --output-dir "$DEPLOY_DIR"
 
-# 5. PATCH Section was REMOVED as it was incorrect
+# 5. Create symlinks for class data instead of copying
+echo "Creating symlinks for class data in $DEPLOY_DIR/files/class_data..."
+mkdir -p "$DEPLOY_DIR/files/class_data"
 
-# 6. Clean up temporary staging directory ONLY
+# Remove any existing class_data content to avoid conflicts
+rm -rf "$DEPLOY_DIR/files/class_data"/*
+
+# Create symlinks for all content in the class_data directory
+for file in "$DATA_SRC_DIR"/*; do
+    if [ -d "$file" ]; then
+        # For directories, create the directory and symlink contents
+        dirname=$(basename "$file")
+        mkdir -p "$DEPLOY_DIR/files/class_data/$dirname"
+        
+        # Symlink files within the directory
+        for subfile in "$file"/*; do
+            ln -s "$(readlink -f "$subfile")" "$DEPLOY_DIR/files/class_data/$dirname/$(basename "$subfile")"
+        done
+    else
+        # For regular files, create symlink
+        ln -s "$(readlink -f "$file")" "$DEPLOY_DIR/files/class_data/$(basename "$file")"
+    fi
+done
+
+# 6. Create lowercase symlinks for notebooks for better compatibility
+echo "Creating lowercase symlinks for notebooks..."
+for notebook in "$DEPLOY_DIR/files"/Lab*.ipynb; do
+    if [ -f "$notebook" ]; then
+        # Create lowercase version of the filename
+        lowercase_name=$(basename "$notebook" | tr '[:upper:]' '[:lower:]')
+        ln -s "$(basename "$notebook")" "$DEPLOY_DIR/files/$lowercase_name"
+    fi
+done
+
+# 7. Clean up temporary staging directory ONLY
 echo "Cleaning up temporary staging directory ($STAGING_DIR)..."
 rm -rf "$STAGING_DIR"
 
